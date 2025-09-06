ec7369f0ce2dad012709e53bbc4524c14c1e3973
William <williamlg@free.fr>
2025-09-06 17:36:58 +0200

    feat(auth)(header): config auth routes + add auth system + add dynamic header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/transactions',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  standalone: true,
+  imports: [RouterOutlet, Header],
+  template: `
+    <app-header></app-header>
+    <main class="container mx-auto p-4">
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  styles: [],
 })
 export class App {
-  protected readonly title = signal('efrei-angular-project');
+  title = 'easy-budget-manager';
 }
