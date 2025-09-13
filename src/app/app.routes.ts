a870a2b39539d32985c4cf4965c150dbab62b4cb
William <williamlg@free.fr>
2025-09-13 15:14:27 +0200

    feat: add auth guard and admin guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,26 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guards';
+import { adminGuard } from './core/guards/admin.guards';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/transactions',
-    pathMatch: 'full',
-  },
-  {
-    path: 'auth',
-    loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
-  },
-  {
-    path: 'transactions',
-    // canActivate: [authGuard],
-    loadChildren: () =>
-      import('./features/transactions/transactions.routes').then((m) => m.TRANSACTION_ROUTES),
-  },
+    {
+        path: '',
+        redirectTo: '/transactions',
+        pathMatch: 'full',
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+    },
+    {
+        path: 'transactions',
+        canActivate: [authGuard],
+        loadChildren: () =>
+            import('./features/transactions/transactions.routes').then((m) => m.TRANSACTION_ROUTES),
+    },
+    {
+        path: 'admin',
+        canActivate: [authGuard, adminGuard],
+        loadChildren: () => import('./features/admin/admin.routes').then((m) => m.ADMIN_ROUTES),
+    },
 ];
