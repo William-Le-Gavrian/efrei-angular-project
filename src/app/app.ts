682cee6d12f5f4b4a0f040607d34942ef5fcbdb0
William <williamlg@free.fr>
2025-09-20 18:42:13 +0200

    feat: add pwa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header/header';
+import { PwaPromptComponent } from './shared/components/pwa-prompt/pwa-prompt.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, Header],
-  template: `
-    <app-header></app-header>
-    <main class="container mx-auto p-4">
-      <router-outlet></router-outlet>
-    </main>
-  `,
-  styles: [],
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, Header, PwaPromptComponent],
+    template: `
+        <app-header></app-header>
+        <main class="container mx-auto p-4">
+            <router-outlet></router-outlet>
+        </main>
+        <app-pwa-prompt></app-pwa-prompt>
+    `,
+    styles: [],
 })
 export class App {
-  title = 'easy-budget-manager';
+    title = 'easy-budget-manager';
 }
