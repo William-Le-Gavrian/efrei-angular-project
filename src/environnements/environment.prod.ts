57c8528920654974036c26ff463ae217c8b154bc
William <williamlg@free.fr>
2025-09-21 17:15:11 +0200

    fix: empty login fields

--- src/environnements/environment.prod.ts
+++ src/environnements/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:4200/',
-  appName: 'EasyBudget Manager',
+    production: false,
+    apiUrl: 'http://localhost:4200/',
+    appName: 'EasyBudget Manager',
+    apiKey: '9132a72795e94d8abd6d9974e20f167c',
 };
